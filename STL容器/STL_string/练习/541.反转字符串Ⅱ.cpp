c39dde5eb9d5e85cbb8e47b16fb4de2062a6449d
PangHahMan <3278475092@qq.com>
2023-04-04 18:15:33 +0800

    1

---
+++ STL容器/STL_string/练习/541.反转字符串Ⅱ.cpp
@@ -0,0 +1,18 @@
+/* 给定一个字符串 s 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
+如果剩余字符少于 k 个，则将剩余字符全部反转。
+如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。 */
+#include<string>
+using namespace std;
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.length();
+        for (int i = 0; i < n; i += 2 * k) {
+            reverse(s.begin() + i, s.begin() + min(i+k,n));   //i+k如果大于n就越界取n即可
+        }
+        return s;
+    }
+};
+
+
+//https://leetcode.cn/problems/reverse-string-ii/
