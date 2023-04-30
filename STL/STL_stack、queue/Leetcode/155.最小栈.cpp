98308b184c685c5d972d5ac59d7a4a925c1a2017
PangHahMan <3278475092@qq.com>
2023-04-30 16:14:13 +0800

    1

---
+++ STL/STL_stack、queue/Leetcode/155.最小栈.cpp
@@ -0,0 +1,36 @@
+#include<stack>
+using namespace std;
+class MinStack
+{
+public:
+    MinStack()
+    {
+        min_stack.push(INT_MAX);
+    }
+
+    void push(int val)
+    {
+        x_stack.push(val);
+        min_stack.push(min(min_stack.top(), val));
+    }
+
+    void pop()
+    {
+        x_stack.pop();
+        min_stack.pop();
+    }
+
+    int top()
+    {
+        return x_stack.top();
+    }
+
+    int getMin()
+    {
+        return min_stack.top();
+    }
+
+private:
+    stack<int> x_stack;
+    stack<int> min_stack;
+};
