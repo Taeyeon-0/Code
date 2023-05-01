91d2a15387b962734edda18e5512b3312694e4f2
PangHahMan <3278475092@qq.com>
2023-05-01 16:08:19 +0800

    priority_queue

---
+++ STL/STL_priority_queue/test.cpp
@@ -0,0 +1,26 @@
+#include "priority_queue.h"
+#include <iostream>
+
+void test()
+{
+    phw::priority_queue<int> p;
+    p.push(81);
+    p.push(12);
+    p.push(32);
+    p.push(85);
+    p.push(80);
+    p.push(19);
+
+    while (!p.empty())
+    {
+        cout << p.top() << " ";
+        p.pop();
+    }
+    cout << endl;
+}
+
+int main()
+{
+    test();
+    return 0;
+}
