dc0d2bbf6a70561597bf8fc0a07d8b5ae2e0146f
PangHahMan <3278475092@qq.com>
2023-06-07 15:59:42 +0800

    1

--- C++11/test.cpp
+++ C++11/test.cpp
@@ -1,3 +1,23 @@
-//
-// Created by 32784 on 6/7/2023.
-//
+#include <iostream>
+using namespace std;
+
+struct Point {
+    int _x;
+    int _y;
+};
+
+int main() {
+    int x1 = 1;
+    int x2 = {2};
+
+
+    //省略赋值符号
+    int x3{3};
+    int array1[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int array2[5]{0};
+    Point p{1, 2};
+
+    //C++11中列表初始化也可以适用于new表达式中
+    int *pa = new int[4]{0};
+    return 0;
+}
