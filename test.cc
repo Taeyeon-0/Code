06fd356b21068992d97175db9084d3e45d44c82d
PangHahMan <3278475092@qq.com>
2023-06-28 19:21:09 +0800

    1

--- test.cc
+++ test.cc
@@ -1,22 +1,60 @@
 #include <iostream>
+using namespace std;
+// class HeapOnly {
+// public:
+//     static void Delete(HeapOnly *p) {
+//         delete p;
+//     }
+
+//     //也可以这样delete
+//     void Delete() {
+//         delete this;
+//     }
+
+// private:
+//     //析构函数私有
+//     ~HeapOnly() {
+//         cout << "~HeapOnly()" << endl;
+//     }
+
+// private:
+//     int _a;
+// };
+
+
+// int main() {
+//     //HeapOnly hp1;
+//     //static HeapOnly hp2;
+//     HeapOnly *ptr = new HeapOnly;
+//     ptr->Delete(ptr);
+//     ptr->Delete();
+
+//     //delete ptr;
+//     return 0;
+// }
+
+
+class HeapOnly {
+public:
+    //提供一个公有的，获取对象的方式,对象控制是new出来的，保证在堆上
+    static HeapOnly *CreateObj() {
+        return new HeapOnly();
+    }
+
+    HeapOnly(const HeapOnly &hp) = delete;
+    HeapOnly &operator=(const HeapOnly &hp) = delete;
+private:
+
+    //方式二、构造函数私有
+    HeapOnly()
+        : _a(0) {}
+
+private:
+    int _a;
+};
 
 int main() {
-    int x = 0;
-
-    auto lambda = [x]() mutable {
-        x++; // 修改被捕获的变量x
-        std::cout << "Value of x: " << x << std::endl;
-    };
-    
-   /*  auto lambda = [&x]() {
-        x++; // 修改被捕获的变量x
-        std::cout << "Value of x: " << x << std::endl;
-    } */
-    
-    lambda(); // 输出: Value of x: 1
-    lambda(); // 输出: Value of x: 2
-
-    std::cout << "Final value of x: " << x << std::endl; // 输出: Final value of x: 0
-
+    HeapOnly *sp4 = HeapOnly::CreateObj();
+    delete sp4;
     return 0;
 }
