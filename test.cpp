3478dedd5a29a08638f8f67370bde0c10cb145d7
PangHahMan <3278475092@qq.com>
2023-08-30 12:48:15 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,21 +1,25 @@
-void AdjustDown(int n, int parent) {
-    int child = 2 * parent + 1;
-    while (child < n) {
-        //_comp(_con[child], _con[child + 1])表示child<child+1
-        if (child + 1 < n && _comp(_con[child], _con[child + 1])) {
-            child++;
-        }
-        // parent<child
-        if (_comp(_con[parent], _con[child]))// 通过所给比较方式确定是否需要交换结点位置
-        {
-            // 将父结点与孩子结点交换
-            swap(_con[child], _con[parent]);
-            // 继续向下进行调整
-            parent = child;
-            child = 2 * parent + 1;
-        } else// 已成堆
-        {
-            break;
-        }
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    virtual void f() {
+        cout << "A::f()" << endl;
     }
+};
+
+class B : public A {
+private:
+    virtual void f() {
+        cout << "B::f()" << endl;
+    }
+};
+
+int main() {
+    A *pa = (A *) new B;// 创建B类的实例并赋值给A类指针
+
+    pa->f();  // 调用虚函数f()，输出结果会根据实际对象类型动态决定
+    delete pa;// 释放内存
+
+    return 0;
 }
